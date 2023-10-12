7af2da3da5f1ddd6cc01d92e68e6e928d0c9dadb
Jimyung Park <jp4453@cumc.columbia.edu>
2023-10-12 13:59:40 -0400

    add few-shot example

--- run_api.py
+++ run_api.py
@@ -23,12 +23,6 @@ def run_re(output_dir, few_shot = True):
     model = config['openai']['model']
     temp = float(config['openai']['temperature'])
     
-    ### Get prompt design
-    if few_shot == True:
-        system_msg = config['RE']['few_prompt']
-    else:
-        system_msg = config['RE']['zero_prompt']
-
     # Create folder store output
     date_path = "output_" + date.today().strftime("%y%m%d") + "/re"
     path = os.path.join(output_dir, date_path)
@@ -37,30 +31,66 @@ def run_re(output_dir, few_shot = True):
     # Read NER input data
     notes = glob.glob(os.path.join(output_dir, 'data/re', '*.txt'))
     
-    for note in td.tqdm(notes, desc="Generate RE output from i2b2", unit="files"):
-        with open(note, 'r') as f:
-            context = f.read()
+    ### Get prompt design
+    if few_shot == True:
+        system_msg = config['RE']['few_prompt']
+        few_user = config['RE']['few_user']
+        few_assistant = config['RE']['few_assistant']
         
-        # GPT API call
-        completions = openai.ChatCompletion.create(
-            model = model,
-            temperature = temp,
-            n = 1,
-            messages = [
-                {'role':'system', 'content':system_msg},
-                {'role':'user', 'content':context}
-            ]
-        )
-        response = completions.choices[0]['message']['content']
-        # Add XML tags. May need to consider processing only the last TLINK sentence.
-        if re.search(r'"\s?/>$', response[-25:]):
-            response = '<TAGS>\n' + response + '\n</TAGS>'
-        else: 
-            response = '<TAGS>\n' + response + '" />' + '\n</TAGS>'
+        for note in td.tqdm(notes, desc="Generate RE output from i2b2", unit="files"):
+            with open(note, 'r') as f:
+                context = f.read()
+            
+            # GPT API call
+            completions = openai.ChatCompletion.create(
+                model = model,
+                temperature = temp,
+                n = 1,
+                messages = [
+                    {'role':'system', 'content':system_msg},
+                    {'role':'user', 'content':few_user},
+                    {'role':'assistant', 'content':few_assistant},
+                    {'role':'user', 'content':context}
+                ]
+            )
+            response = completions.choices[0]['message']['content']
+            # Add XML tags. May need to consider processing only the last TLINK sentence.
+            if re.search(r'"\s?/>$', response[-25:]):
+                response = '<TAGS>\n' + response + '\n</TAGS>'
+            else: 
+                response = '<TAGS>\n' + response + '" />' + '\n</TAGS>'
+            
+            output_file = os.path.join(path, os.path.splitext(os.path.basename(note))[0] + '.xml')
+            with open(output_file, 'w', encoding='utf-8') as f:
+                f.write(response)
         
-        output_file = os.path.join(path, os.path.splitext(os.path.basename(note))[0] + '.xml')
-        with open(output_file, 'w', encoding='utf-8') as f:
-            f.write(response)
+    else:
+        system_msg = config['RE']['zero_prompt']
+        
+        for note in td.tqdm(notes, desc="Generate RE output from i2b2", unit="files"):
+            with open(note, 'r') as f:
+                context = f.read()
+            
+            # GPT API call
+            completions = openai.ChatCompletion.create(
+                model = model,
+                temperature = temp,
+                n = 1,
+                messages = [
+                    {'role':'system', 'content':system_msg},
+                    {'role':'user', 'content':context}
+                ]
+            )
+            response = completions.choices[0]['message']['content']
+            # Add XML tags. May need to consider processing only the last TLINK sentence.
+            if re.search(r'"\s?/>$', response[-25:]):
+                response = '<TAGS>\n' + response + '\n</TAGS>'
+            else: 
+                response = '<TAGS>\n' + response + '" />' + '\n</TAGS>'
+            
+            output_file = os.path.join(path, os.path.splitext(os.path.basename(note))[0] + '.xml')
+            with open(output_file, 'w', encoding='utf-8') as f:
+                f.write(response)
 
 def run_nerre():
     pass
