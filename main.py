c2dce8a77138a95ca68ccdc6e9072fa4352b8af5
Jimyung Park <jp4453@cumc.columbia.edu>
2023-10-26 12:28:48 -0400

    udpate main process

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import logging, logging.handlers
-from configparser import ConfigParser
+import configparser
 import os
 
 from generate_data import generate_eval_data, generate_input_data
-from eval import eval_re
-from run_api import run_re
+from eval import eval_ner, eval_re, eval_nerre
+from run_api import run_ner, run_re, run_nerre
 
 from datetime import datetime as date
 
-os.chdir('/phi_home/jp4453/Temporal-Phenotype')
+os.chdir('/Users/jimmypark/Documents/git/Temporal-Phenotype')
 file_log = './tRE_' + date.today().strftime("%y%m%d") + '.log' 
 
 if __name__ == "__main__":
@@ -26,44 +26,152 @@ if __name__ == "__main__":
         logger.addHandler(fh)
         logger.addHandler(ch)
         
-        logger.info('===============================================')
-        logger.info('Begin temporal relation extraction')
-        
         # Set input and output directories
-        input_dir = os.path.join(os.getcwd(), 'i2b2-2012-original') 
+        input_dir = os.path.join(os.getcwd(), 'i2b2-2012-original')
+        if os.path.exists(input_dir):
+            logging.error(f'i2b2-2012-original directory does not exist. Please uploaed data or change directory name to i2b2-2012-original')
         output_dir = os.path.join(os.getcwd(), 'result') 
+        
+        logger.info('============================================================')
         logger.info(f'i2b2 data path: {input_dir}')
         logger.info(f'output directory path: {output_dir}')
         
-        # Generate and transform i2b2 data
-        logger.info('Transform i2b2-2012 data for tasks')
-        generate_input_data(input_dir, output_dir)
+        # Generate and transform i2b2 data        
+        if not os.path.exists(os.path.join(output_dir, 'data')):
+            logger.info('converting i2b2-2012 data for tasks...')
+            generate_input_data(input_dir, output_dir)
+            logger.info(f'done converting i2b2-2012')
+        else:
+            logger.info(f'skip convering i2b2 data. data directory already exists in {output_dir}')
         
         # Generate eval datasets
-        logger.info('Generate eval data from i2b2-2012')
-        generate_eval_data(input_dir, output_dir)
+        if not os.path.exists(os.path.join(output_dir, 'eval')):
+            logger.info(f'generate eval data from i2b2-2012...')
+            generate_eval_data(input_dir, output_dir)
+            logger.info(f'done generating eval data')
+        else:
+            logger.info(f'skip convering eval data. eval directory already exists in {output_dir}')
         
-        # Run GPT API function
-        logger.info('Run GPT API for the temporal relation extraction')
-        run_re(output_dir, few_shot = False)
+        ### Run GPT API function
+        ## NER
+        # few shot
+        logger.info('============================================================')
+        try:
+            logger.info(f'start few-shot ner...')
+            run_ner(output_dir, few_shot = True, api_retry=6)
+            logger.info(f'done few-shot ner')
+        except Exception as e:
+            logger.info(f'error occurred while running few-shot ner: {e}')
+        # zero shot
+        try:
+            logger.info(f'start one-shot ner...')
+            run_ner(output_dir, few_shot = False, api_retry=6)
+            logger.info(f'done one-shot ner')
+        except Exception as e:
+            logger.info(f'error occurred while running one-shot ner: {e}')
+        ## tRE
+        # few shot
+        try:
+            logger.info(f'start one-shot tre...')
+            run_re(output_dir, few_shot = True, api_retry=6)
+            logger.info(f'done one-shot tre')
+        except Exception as e:
+            logger.info(f'error occurred while running one-shot tre: {e}')
+        # zero shot
+        try:
+            logger.info(f'start zero-shot tre...')
+            run_re(output_dir, few_shot = False, api_retry=6)
+            logger.info(f'done zero-shot tre')
+        except Exception as e:
+            logger.info(f'error occurred while running zero-shot tre: {e}')
+        ## NER-RE
+        # few shot
+        try:
+            logger.info(f'start one-shot ner-re...')
+            run_nerre(output_dir, few_shot = True, api_retry=6)
+            logger.info(f'done one-shot ner-re')
+        except Exception as e:
+            logger.info(f'error occurred while running one-shot ner-re: {e}')
+        # zero shot
+        try:
+            logger.info(f'start zero-shot ner-re...')
+            run_nerre(output_dir, few_shot = False, api_retry=6)
+            logger.info(f'done zero-shot ner-re')
+        except Exception as e:
+            logger.info(f'error occurred while running zero-shot ner-re: {e}')
         
         # Evaluate GPT output
-        logger.info('Evaluate tRE output')
-        micro_precision, micro_recall, micro_f1, macro_precision, macro_recall, macro_f1 = eval_re(output_dir, execute_date = None)
+        logger.info('============================================================')
+        config = configparser.ConfigParser()
+        config.read(os.path.join(os.getcwd(), "api.config"))
+        model = config['openai']['model']
+        one_basic_path = "output_" + "one_" + config['openai']['model'] + '_' + date.today().strftime("%y%m%d")
+        zero_basic_path = "output_" + "zero_" + config['openai']['model'] + '_' + date.today().strftime("%y%m%d")
         
-        logger.info(f'tRE macro precision: {macro_precision}')
-        logger.info(f'tRE macro recall: {macro_recall}')
-        logger.info(f'tRE macro f1: {macro_f1}')
-        logger.info(f'tRE micro precesion: {micro_precision}')
-        logger.info(f'tRE micro recall: {micro_recall}')
-        logger.info(f'tRE micro f1: {micro_f1}')
+        # one-shot ner
+        if os.path.exists(os.path.join(output_dir, one_basic_path, 'ner')):
+            logger.info('evaluate one-shot ner output')
+            try:
+                eval_df = eval_ner(output_dir, few_shot=True, execute_date = None)
+                eval_df.to_csv(os.path.join(output_dir, one_basic_path, 'ner_one_df.csv'), index=False)
+            except Exception as e:
+                logger.error(f'error occurred while evaluating one-shot ner: \n{e}')
+        else:
+            logger.info(f'pass evaluating one-shot ner')
+        # zero-shot ner
+        if os.path.exists(os.path.join(output_dir, zero_basic_path, 'ner')):
+            logger.info('evaluate zero-shot ner output')
+            try:
+                eval_df = eval_ner(output_dir, few_shot=False, execute_date = None)
+                eval_df.to_csv(os.path.join(output_dir, one_basic_path, 'ner_zero_df.csv'), index=False)
+            except Exception as e:
+                logger.error(f'error occurred while evaluting zero-shot ner: \n{e}')
+        else:
+            logger.info(f'pass evaluating zero-shot ner output')
+            
+        # one-shot re
+        if os.path.exists(os.path.join(output_dir, one_basic_path, 're')):
+            logger.info('evaluate one-shot tre output')
+            try:
+                eval_df = eval_re(output_dir, few_shot=True, execute_date = None)
+                eval_df.to_csv(os.path.join(output_dir, one_basic_path, 're_one_df.csv'), index=False)
+            except Exception as e:
+                logger.error(f'error occurred while evaluating one-shot tre: \n{e}')
+        else:
+            logger.info(f'pass evaluating one-shot tre')
+        # zero-shot re
+        if os.path.exists(os.path.join(output_dir, zero_basic_path, 're')):
+            logger.info('evaluate zero-shot tre output')
+            try:
+                eval_df = eval_re(output_dir, few_shot=False, execute_date = None)
+                eval_df.to_csv(os.path.join(output_dir, one_basic_path, 're_zero_df.csv'), index=False)
+            except Exception as e:
+                logger.error(f'error occurred while evaluating zero-shot tre: \n{e}')
+        else:
+            logger.info(f'pass evaluating zero-shot tre output')
         
-        # Neeed to be implemented
-        # Standardize extracted temporal related concepts
-                
+        # one-shot ner-re
+        if os.path.exists(os.path.join(output_dir, one_basic_path, 'nerre')):
+            logger.info('evaluate one-shot ner-re output')
+            try:
+                eval_df = eval_nerre(output_dir, few_shot=True, execute_date = None)
+                eval_df.to_csv(os.path.join(output_dir, one_basic_path, 'nerre_one_df.csv'), index=False)
+            except Exception as e:
+                logger.error(f'error occurred while evaluating one-shot ner-re: \n{e}')
+        else:
+            logger.info(f'pass evaluating one-shot ner')
+        # zero-shot ner
+        if os.path.exists(os.path.join(output_dir, zero_basic_path, 'ner')):
+            logger.info('evaluate zero-shot ner-re output')
+            try:
+                eval_df = eval_nerre(output_dir, few_shot=False, execute_date = None)
+                eval_df.to_csv(os.path.join(output_dir, one_basic_path, 'nerre_one_df.csv'), index=False)
+            except Exception as e:
+                logger.error(f'error occurred while evaluting zero-shot ner-re: \n{e}')
+        else:
+            logger.info(f'pass evaluating zero-shot ner-re output')
         
         logger.info('Done!')
 
     except Exception as e:
-        logger.exception(e)
-        
+        logger.exception(e)       
